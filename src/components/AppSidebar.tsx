05fab77016e482f8f134d5e97a789d644248557f
Akshay-Anand-Code <akshay1079410@gmail.com>
2024-12-03 16:52:05 +0530

    sidebar

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -70,13 +70,13 @@ const AppSidebar = () => {
               <div className="app-sidebar__logo-container group">
                 <div className="app-sidebar__logo-wrapper">
                   <Image
-                    src="/logo.svg"
+                    src="/logo.png"
                     alt="logo"
                     width={25}
                     height={20}
                     className="app-sidebar__logo"
                   />
-                  <p className="app-sidebar__title">EDROH</p>
+                  <p className="app-sidebar__title">ACQUIRE</p>
                 </div>
                 <PanelLeft className="app-sidebar__collapse-icon" />
               </div>
