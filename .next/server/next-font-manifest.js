88fff121b2f7ea8398175f2c6ff10f604ec3c0ff
Akshay-Anand-Code <akshay1079410@gmail.com>
2024-12-03 16:47:50 +0530

    sidebar

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/cairo/ac-land/src/app/layout\":[\"static/media/7e6a2e30184bb114-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
